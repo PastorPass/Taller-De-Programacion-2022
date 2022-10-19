52bbda06335e26e490671b2217337168caff0807
Shepper <89154301+PastorPass@users.noreply.github.com>
2022-10-19 18:06:33 -0300

    Create Compra.java

---
+++ Modulo Objetos/Parciales Objetos Taller/Soluciones de Parciales/parcial 26/Compra.java
@@ -0,0 +1,12 @@
+
+package Parciales;
+
+public abstract class Compra {
+    public Compra () {
+        
+    }
+    public abstract double precioApagar(int unaPos);
+    public abstract void agregarCompra(Producto p);
+    
+
+}
